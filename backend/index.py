4bd252ba135585f644d9079593889c616871e544
manikdialani <manikdialani@gmail.com>
2021-03-09 10:27:05 -0800

    testing backend patientID

--- backend/index.py
+++ backend/index.py
@@ -241,6 +241,7 @@ def handler(event, context):
     logger.info(f'Key for JSON is {json_key}')
     logger.info(f'Amplify User is: {amplify_user}')
     logger.info(f'Json Content is {json_content}')
+    logger.info(f'Patient ID is {int(json_content["confidence"])}')
     #logger.info(f'Patient ID is {int(json_content["PatientID"])}')
     # Get contents of JSON 
     key = json_content["key"]
