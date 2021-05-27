ab74ad02c50d911bcc9fd58f47caaf08abdc068a
manikdialani <manikdialani@gmail.com>
2021-05-27 09:55:38 -0700

    add MIT License

--- setup_athena.sh
+++ setup_athena.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 
-BUCKET=$(aws resourcegroupstaggingapi get-resources --tag-filters Key=user:Application,Values="ibdcentre" --resource-type-filters s3 --query 'ResourceTagMappingList[*].[ResourceARN]' --output text | grep -v deployment | awk -F':::' '{print $2}')
-if [ -z "$BUCKET" ]; then
-    echo 'Unable to find S3 BUCKET'
-    exit 1
-fi
-echo "Bucket Name: ${BUCKET}"
+echo "Enter Bucket Name: ${BUCKET}"
+read BUCKET
 
 echo -e "Now creating Athena Database..."
-aws athena start-query-execution --query-string file://athenaDb.sql --query-execution-context Database="default" --result-configuration OutputLocation=s3://ibdcentreb3cbdd9c0c2a495498c88bf5c73a4aea141302-dev/ath-output
+aws athena start-query-execution --query-string file://athenaDb.sql --query-execution-context Database="default" --result-configuration OutputLocation=s3://$BUCKET/ath-output
 echo -e "Creating Athena Json Table"
-sed "s/<app-bucket>/ibdcentreb3cbdd9c0c2a495498c88bf5c73a4aea141302-dev/" athenaTable.sql > athenaTable.json.sql
-aws athena start-query-execution --query-string file://athenaTable.json.sql --query-execution-context Database="ibd_records" --result-configuration OutputLocation=s3://ibdcentreb3cbdd9c0c2a495498c88bf5c73a4aea141302-dev/ath-output
+sed "s/<app-bucket>/${BUCKET}/" athenaTable.sql > athenaTable.json.sql
+aws athena start-query-execution --query-string file://athenaTable.json.sql --query-execution-context Database="ibd_records" --result-configuration OutputLocation=s3://$BUCKET/ath-output
 rm athenaTable.json.sql
